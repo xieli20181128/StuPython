fd4168e0988894d822d2ee1a4f648d7263c4d7ae
xieli20181128 <xl85332113>
2019-08-08 16:26:30 +0800

    第15次作业

---
+++ 15-Selenium-Conclu.py
@@ -0,0 +1,16 @@
+#-*- coding: utf-8 -*-
+#
+# 1.课程总结和常见问题处理
+#
+# 必备：
+# 初级：html、css、js、http
+# 中级：Linux、mysql、jmeter（压力测试用的比较多的）、lr（用的比较少）
+#
+# python+requests  网络库去开发接口自动化测试
+#
+# 高级：Jenkins CI、git(管理项目质量，搭建所有基础组件，bug管理工具)
+#
+#
+#
+# 可选：
+# python、java
